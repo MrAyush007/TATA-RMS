35d45555e39e8ad79b5ee050a6a00b0b69d1db77
Gupta <AGUPTA90@jaguarlandrover.com>
2024-12-09 16:42:37 +0530

    second

--- src/test/java/com/app/ApplicationTests.java
+++ src/test/java/com/app/ApplicationTests.java
@@ -1,10 +1,10 @@
-package com.TataRMS.recruitment_System;
+package com.app;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class RecruitmentSystemApplicationTests {
+class ApplicationTests {
 
 	@Test
 	void contextLoads() {
