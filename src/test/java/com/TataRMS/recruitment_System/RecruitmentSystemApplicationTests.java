8a84dc3eb88f05c2d54b6fff7ad6129ef8cfc03b
Gupta <AGUPTA90@jaguarlandrover.com>
2024-12-09 16:26:49 +0530

    first

---
+++ src/test/java/com/TataRMS/recruitment_System/RecruitmentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TataRMS.recruitment_System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecruitmentSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
