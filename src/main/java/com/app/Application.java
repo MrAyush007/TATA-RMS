b5df9a1e9af6e7516a420e3503234a205b0a5160
Gupta <AGUPTA90@jaguarlandrover.com>
2024-12-10 17:33:19 +0530

    pdfext_done

--- src/main/java/com/app/Application.java
+++ src/main/java/com/app/Application.java
@@ -1,3 +1,31 @@
+//package com.app;
+//
+//import org.modelmapper.Conditions;
+//import org.modelmapper.ModelMapper;
+//import org.modelmapper.convention.MatchingStrategies;
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.context.annotation.Bean;
+//
+//@SpringBootApplication
+//public class Application {
+//
+//	public static void main(String[] args) {
+//		SpringApplication.run(Application.class, args);
+//	}
+//
+//	@Bean // equivalent to <bean id ..../> in xml file
+//	public ModelMapper mapper() {
+//		ModelMapper modelMapper = new ModelMapper();
+//		modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT)
+//				.setPropertyCondition(Conditions.isNotNull());
+//		return modelMapper;
+//	}
+//
+//}
+
+
+
 package com.app;
 
 import org.modelmapper.Conditions;
@@ -5,9 +33,10 @@ import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class Application {
 
 	public static void main(String[] args) {
@@ -21,5 +50,4 @@ public class Application {
 				.setPropertyCondition(Conditions.isNotNull());
 		return modelMapper;
 	}
-
 }
